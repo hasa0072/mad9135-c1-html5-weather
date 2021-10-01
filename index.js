84178946cc97f6642933487c12ef62c12f21d3a3
Sevda Hasanli <hasa0072@algonquinlive.com>
2021-10-01 02:15:04 -0400

    Hourly section is functional

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function main() {
     console.log(forecast);
 
     updateCurrentTempHTML(location, forecast)
+    updateHourlyTempHTML(forecast)
   } catch (error) {
     console.log(error.message);
   }
@@ -35,6 +36,26 @@ function updateCurrentTempHTML(location, forecast) {
   document.getElementById("low").innerHTML = lo;
 }
 
+function updateHourlyTempHTML(forecast) {
+  // get next 6 hours of forecasts
+  const N = 6;
+  let fcs = getNextHoursForecast(forecast, N);
+  // update hours row
+  let hoursContainer = document.getElementById("hours");
+  // let innerHTML = forecastToHourlyHTML(fcs[0]);
+  let innerHTML = forecastToHourlyHTML(forecast.current)
+  fcs.forEach((fc) => {
+    innerHTML += forecastToHourlyHTML(fc)
+  })
+
+  hoursContainer.innerHTML = innerHTML
+}
+
+/*
+ * Utility functions below this line
+ */
+
+// Finds highest and lowest temperature for today's forecast
 function getHighestAndLowestTemps(forecast) {
   let hi = -300; // below absolute zero is not possible, safe to use -300
   let lo = 100000; // not a realistic lo value, safe to use 100000 degrees for initial value
@@ -49,4 +70,56 @@ function getHighestAndLowestTemps(forecast) {
   })
 
   return {hi: hi, lo: lo}
+}
+
+// Get next N hours of forecast from now
+function getNextHoursForecast(forecast, n) {
+  let fcs = []
+  let count = n
+  forecast.hourly.forEach((fc) => {
+    // if we have enough forecasts, return
+    if (count == 0) {
+      return
+    }
+    // skip forecasts before current time
+    if (fc.dt <= forecast.current.dt) {
+      return
+    }
+
+    // add until we have n forecasts
+    fcs.push(fc)
+    count -= 1
+  })
+
+  return fcs
+}
+
+function forecastToHourlyHTML(forecast) {
+  let temp = forecast.temp
+  let status = 's'
+  let date = new Date(forecast.dt * 1000)
+  let hour = date.getHours()
+  if (hour < 12) {
+    hour = `${hour}AM`
+  } else if (hour == 12) {
+    hour = `${hour}PM`
+  } else {
+    hour -= 12
+    hour = `${hour}PM`
+  }
+  return `
+  <div class="col-2">
+    <div class="d-flex flex-column">
+      <div id="h-hour">
+        ${hour}
+      </div>
+      <div id="h-status">
+        ${status}
+      </div>
+      <div id="h-temp">
+        ${temp}
+      </div>
+    </div>
+  </div>
+  `
 }
